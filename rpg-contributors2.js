9c89d42f75a1ccd98973d99e00151b3eac589339
LukeCig33 <luke.cignetti@gmail.com>
2025-03-16 14:20:34 -0400

    First Changes

--- rpg-contributors2.js
+++ rpg-contributors2.js
@@ -5,6 +5,7 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import '@haxtheweb/rpg-character/rpg-character.js';
 
 /**
  * `rpg-contributors2`
@@ -26,6 +27,8 @@ export class RpgContributors2 extends DDDSuper(I18NMixin(LitElement)) {
       ...this.t,
       title: "Title",
     };
+    this.contributors = [];
+    this.getData();
     this.registerLocalization({
       context: this,
       localesPath:
@@ -38,10 +41,13 @@ export class RpgContributors2 extends DDDSuper(I18NMixin(LitElement)) {
   // Lit reactive properties
   static get properties() {
     return {
-      ...super.properties,
       title: { type: String },
+      loading: { type: Boolean, reflect: true },
+      items: { type: Array, },
+      value: { type: String },
     };
   }
+
 
   // Lit scoped styles
   static get styles() {
@@ -63,14 +69,64 @@ export class RpgContributors2 extends DDDSuper(I18NMixin(LitElement)) {
     `];
   }
 
+  constructor() {
+    super();
+    this.value = null;
+    this.title = '';
+    this.loading = false;
+    this.items = [];
+  }
+
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+    <h2>${this.title}</h2>
+    <details open>
+      <summary>Search inputs</summary>
+      <div>
+        <input id="input" placeholder="Search NASA images" @input="${this.inputChanged}" />
+      </div>
+    </details>
+    <div class="results">
+      ${this.items.map((item, index) => html`
+      <nasa-image
+        source="${item.links[0].href}"
+        title="${item.data[0].title}"
+      ></nasa-image>
+      `)}
+    </div>
+    `;
   }
+
+  inputChanged(e) {
+    this.value = this.shadowRoot.querySelector('#input').value;
+  }
+  // life cycle will run when anything defined in `properties` is modified
+  updated(changedProperties) {
+    // see if value changes from user input and is not empty
+    if (changedProperties.has('value') && this.value) {
+      this.updateResults(this.value);
+    }
+    else if (changedProperties.has('value') && !this.value) {
+      this.items = [];
+    }
+    // @debugging purposes only
+    if (changedProperties.has('items') && this.items.length > 0) {
+      console.log(this.items);
+    }
+  }
+
+  updateResults(value) {
+    this.loading = true;
+    fetch(`https://api.github.com/repos/haxtheweb/webcomponents/contributors=${value}`).then(d => d.ok ? d.json(): {}).then(data => {
+      if (data.collection) {
+        this.items = [];
+        this.items = data.collection.items;
+        this.loading = false;
+      }  
+    });
+  }
+
 
   /**
    * haxProperties integration via file reference
